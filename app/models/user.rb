0b4b55420294a038cd456029418e6e069007d332
chinatsuosawa <c.osawa@nt-21.co.jp>
2016-11-06 13:01:40 +0000

    want&have method add

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,11 @@ class User < ActiveRecord::Base
   has_many :ownerships , foreign_key: "user_id", dependent: :destroy
   has_many :items ,through: :ownerships
 
+  has_many :wants, class_name: "Want", foreign_key: "user_id", dependent: :destroy
+  has_many :want_items , through: :wants, source: :item
+
+  has_many :haves, class_name: "Have", foreign_key: "user_id", dependent: :destroy
+  has_many :have_items , through: :haves, source: :item
 
   # 他のユーザーをフォローする
   def follow(other_user)
@@ -30,21 +35,33 @@ class User < ActiveRecord::Base
   end
 
   ## TODO 実装
+  # itemをhaveする
   def have(item)
+    have_items.create(followed_id: item.item_id)
   end
 
+  # itemのhaveを解除する
   def unhave(item)
+    have_items.find_by(followed_id: item.item_id).destroy
   end
 
+  # itemをhaveしている場合true、haveしていない場合falseを返す
   def have?(item)
+    have_items.include?(item)
   end
 
+  # itemをwantする
   def want(item)
+    want_items.create(followed_id: item.item_id)
   end
 
+  # itemのwantを解除する
   def unwant(item)
+    want_items.find_by(followed_id: item.item_id).destroy
   end
 
+  # itemをwantしている場合true、wantしていない場合falseを返す
   def want?(item)
+    want_items.include?(item)
   end
 end
